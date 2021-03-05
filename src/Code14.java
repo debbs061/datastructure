6d8a11da2d30e7451da3c1b1d0bb90e800f1a7a9
Kong <debbs061@naver.com>
2021-03-06 08:06:41 +0900

    Bubble sort

---
+++ src/Code14.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+/**
+ * 사용자로부터 n개의 정수를 입력받은 후 오름차순으로 정렬(sort)
+ * 맨 처음부터 두 원소를 비교하며 가장 큰 값이 맨 마지막으로 배치됨.
+ * -> 그리고 그 마지막 배치된걸 제외하고 나머지끼리 다시 비교
+ */
+public class Code14 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int data[] = new int[n];
+        for (int i = 0; i < n; i++) {
+            data[i] = sc.nextInt();
+        }
+
+        for (int i = n - 1; i > 0; i--) { // 인덱스 n-1부터
+            // data[0] .. data[i] -> 에서 가장 큰 값을 data[i]로 넣음
+            for (int j = 0; j < i; j++) {
+                if (data[j] > data[j + 1]) {
+                    // swap data[j] and data[j+1]
+                    int tmp = data[j];
+                    data[j] = data[j+1];
+                    data[j+1] = tmp;
+                }
+            }
+        }
+        System.out.println("Sorted data: ");
+        for (int i = 0; i < n; i++) {
+            System.out.println(data[i]);
+        }
+    }
+}
---
+++ src/Code15.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+/**
+ * Ordered list 에 insert 하는 알고리즘 (Bubble sort 개념 존재)
+ * 현재까지 입력된 정수들을 정렬된 상태로 출력하라
+ */
+public class Code15 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int data[] = new int[n];
+        for (int i = 0; i < n; i++) {
+            int tmp = sc.nextInt();
+            int j = i - 1;
+            // ordered list에 insert 하는 알고리즘은, 뒤에서부터 비교해가며 한칸씩 뒤로 밀려주는게 제일 빠름
+            while (j >= 0 && data[j] > tmp) {
+                data[j + 1] = data[j];
+                j--;
+            }
+            data[j + 1] = tmp;
+            for (int k = 0; k < i + 1; k++) {
+                System.out.print(data[k] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
