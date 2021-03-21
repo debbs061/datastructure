056cd9bfbd4933426faa919ae50652f76287f816
Kong <debbs061@naver.com>
2021-03-22 04:51:50 +0900

    설명 추가

--- src/sorting/InsertionSort.java
+++ src/sorting/InsertionSort.java
@@ -12,12 +12,12 @@ public class InsertionSort {
     // selectionSort, bubbleSort : 이미 정렬이 이루어져 있는 상태여도, 반드시 반복을 수행함 (비효율적)
     // insertionSort : '필요할 때만' 위치를 바꿈. 앞에 있는 원소들이 이미 정렬되어 있다고 가정.
     static void insertionSort(int[] a, int n) {
-        int j;
-        for (int i = 0; i < n; i++) {
-            j = i; // 현재 정렬할 원소를 선택해서 적절한 위치에 삽입하기 위함
-            while (j > 0 && a[j - 1] > a[j]) {
-                swap(a, j - 1, j);
-                j--;
+        for (int i = 1; i < n; i++) {
+            for (int j = i; j > 0; j--) {
+                if (a[j] < a[j - 1])
+                    swap(a, j, j - 1);
+                else
+                    break;
             }
         }
     }
--- src/sorting/SelectionSort.java
+++ src/sorting/SelectionSort.java
@@ -12,12 +12,12 @@ public class SelectionSort {
     // 가장 작은 것을 선택해서 앞으로 보낸다 (exchange)
     // 앞에 있는 원소가 하나씩 줄어드는 방법
     static void selectionSort(int[] a, int n) {
-        for (int i = 0; i < n - 1; i++) { // 가장 작은걸 앞으로 보낸다음 그 앞 원소를 제외하고 다시 선택
-            int min = i;            // 아직 정렬되지 않은 부분에서 가장 작은 요소의 인덱스를 기록합니다.
+        for (int i = 0; i < n - 1; i++) {
+            int min = i;
             for (int j = i + 1; j < n; j++)
                 if (a[j] < a[min])
-                    min = j;
-            swap(a, i, min);        // 아직 정렬되지 않은 부분의 첫 요소와 가장 작은 요소를 교환합니다.
+                    min = j;        // 아직 정렬되지 않은 부분에서 가장 작은 요소의 인덱스를 선택
+            swap(a, i, min);        // 가장 앞의 인덱스와 교환
         }
     }
 
