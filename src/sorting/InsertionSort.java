3724dfe8bade8c810461dd9e0f6049a03e7e82b6
debbs061 <gyr7220@naver.com>
2021-06-08 05:56:11 +0900

    description about Sorting

--- src/sorting/InsertionSort.java
+++ src/sorting/InsertionSort.java
@@ -2,6 +2,8 @@ package sorting;
 
 import java.util.Scanner;
 
+// selectionSort, bubbleSort : 이미 정렬이 이루어져 있는 상태여도, 반드시 반복을 수행함 (비효율적)
+// insertionSort : '필요할 때만' 위치를 바꿈. 앞에 있는 원소들이 이미 정렬되어 있다고 가정.
 public class InsertionSort {
     static void swap(int[] a, int idx1, int idx2) {
         int tmp = a[idx1];
@@ -9,8 +11,7 @@ public class InsertionSort {
         a[idx2] = tmp;
     }
 
-    // selectionSort, bubbleSort : 이미 정렬이 이루어져 있는 상태여도, 반드시 반복을 수행함 (비효율적)
-    // insertionSort : '필요할 때만' 위치를 바꿈. 앞에 있는 원소들이 이미 정렬되어 있다고 가정.
+    // for 문 버전
     static void insertionSort(int[] a, int n) {
         for (int i = 1; i < n; i++) {       // 인덱스 i부터 1까지 1씩 감소하며 반복
             for (int j = i; j > 0; j--) {   // 한 칸씩 왼쪽으로 이동
@@ -18,6 +19,17 @@ public class InsertionSort {
                     swap(a, j, j - 1);
                 else    // 자기보다 작은 데이터를 만나면 그 위치에서 멈춤 (그 앞의 원소들은 이미 정렬되어 있다고 가정하기 때문)
                     break;
+            }
+        }
+    }
+
+    // while 문 버전
+    static void insertionSort2(int a[], int n) {
+        for (int i = 1; i < n; i++) {
+            int j = i;
+            while (j >= 1 && a[j] < a[j - 1]) {
+                swap(a, j, j - 1);
+                j--;
             }
         }
     }
@@ -34,7 +46,7 @@ public class InsertionSort {
             x[i] = sc.nextInt();
         }
 
-        insertionSort(x, n);                // 배열 x를 단순교환정렬
+        insertionSort2(x, n);                // 배열 x를 단순교환정렬
 
         System.out.println("오름차순으로 정렬했습니다.");
         for (int i = 0; i < n; i++)
